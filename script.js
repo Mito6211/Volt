a6a948a1e1325eefdbc385848e5c6afa046c257a
Mito6211 <willmoore674@gmail.com>
2021-01-01 13:08:30 -0500

    Added database + poll functionality

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/polls", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (err) => console.error("DB_ERROR: " + err));
+
+const PollSchema = mongoose.Schema({
+    id: Number,
+    expiration: Number,
+    upvotes: Number,
+    downvotes: Number,
+});
+
+const Poll = mongoose.model("Poll", PollSchema, "pollData");
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+// enter ID of the discord channel you want the polls to occur
 const POLL_CHANNEL_ID = "794356261168087090";
-
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+const MS_IN_A_DAY = 1000 * 60 * 60 * 24;
 
 client.on("message", (msg) => {
     if (msg.content === "!verify") {
-        client.channels.cache.get(POLL_CHANNEL_ID).send(`${msg.author}`).then((message) => {
-            message.react("👍");
-            message.react("👎");
-        });
+        client.channels.cache
+            .get(POLL_CHANNEL_ID)
+            .send(`${msg.author}`)
+            .then((message) => {
+                message.react("👍");
+                message.react("👎");
+                const newPoll = new Poll({
+                    id: message.id,
+                    expiration: new Date().getTime() + MS_IN_A_DAY,
+                    upvotes: 0,
+                    downvotes: 0,
+                });
+
+                newPoll.save((err, poll) => {
+                    if (err) return console.error("SAVE_ERROR: " + err);
+                    console.log(
+                        "Poll with ID " +
+                            poll.id +
+                            " saved to pollData collection."
+                    );
+                });
+            });
     }
 });
 
